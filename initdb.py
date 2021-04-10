a0df4f1999ce975d25a4772114230082c0f9d0b3
ohui <74703654+oscahui@users.noreply.github.com>
2021-04-10 21:43:18 +1000

    Add files via upload

--- initdb.py
+++ initdb.py
@@ -1,47 +1,95 @@
 import os
-from sqlalchemy import create_engine, Table, Column, Float, Integer, String, MetaData
+import csv
+from sqlalchemy import create_engine, Table, Column, MetaData
+from sqlalchemy import DateTime, Float, Integer, String
 
+"""
+This script is used to create the postgres database and
+populate it with data. This is often referred to as 'seeding'.
+"""
+
+"""
+We'll be using the SQLAlchemy ORM approach to defining, creating,
+and inserting into our database. For this we'll need an instance 
+the SQLAlchemy of `MetaData` object - for more detail see:
+https://docs.sqlalchemy.org/en/13/core/metadata.html
+"""
 meta = MetaData()
 
-#postgresql://admin:aa1320@localhost:5432/VIC_crime
+"""
+For deployment purposes we'll be using an environment variable
+for storing the details of our database connection. This way
+we can keep the login and password of our database outside of our
+code.
+"""
+os_env_db_url = os.environ.get('DATABASE_URL', '')
 
+"""
+If we don't have an environment variable 'DATABASE_URL' the value in
+`os_env_db_url` will be an empty string. In which case we will use
+a sqlite database - this allows us to do development without having to
+configure a SQL Database. I wouldn't advise using a sqlite database in
+the case you're expecting multiple concurrent access to the database,
+but for development purposes it should be fine.
+"""
+connection = os_env_db_url or "sqlite:///db.sqlite"
 
-connection = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
-
+"""
+Let's establish a connection to our database.
+"""
 print("connection to databse")
+print("os env", os.environ.get('DATABASE_URL', ''))
 engine = create_engine(connection)
 
-if not engine.has_table("CRIME_LGA"):
+"""
+If the database table already exists we will not be adding to the 
+database.
+"""
+if not engine.has_table("avatar_history"):
     print("Creating Table")
 
+    """
+    Here we'll define the table using the SQLAlchemy ORM interface
+    https://docs.sqlalchemy.org/en/13/core/metadata.html#creating-and-dropping-database-tables
+    """
     new_table = Table(
         'CRIME_LGA', meta,
-        Column('Year', Integer),
+        Column('Year', String),
         Column('Year_ending', String),
         Column('Police_Service_Area', String),
         Column('Local_Government_Area', String),
         Column('Offence_Division', String),
         Column('Offence_Subdivision', String),
         Column('Offence_Subgroup', String),
-        Column('Incidents_Recorded', Float),
-        Column('PSA_Rate_per_100k_population', Float),
-        Column('LGA_Rate_per_100k_population', Float),        
+        Column('Incidents_Recorded', String),
+        Column('PSA_Rate_per_100k_population', String),
+        Column('LGA_Rate_per_100k_population', String),        
     )
 
     meta.create_all(engine)
     
     print("Table created")
 
-    seed_data = [        {"Year":2020,"Year_ending":"December","Police_Service_Area":"Ballarat","Local_Government_Area":"Ballarat","Offence_Division":"A Crimes against the person","Offence_Subdivision":"A10 Homicide and related offences","Offence_Subgroup":"A10 Homicide and related offences","Incidents_Recorded":2,"PSA_Rate_per_100k_population":1.7,"LGA_Rate_per_100k_population":1.8},
-        {"Year":2020,"Year_ending":"December","Police_Service_Area":"Pyrenees","Local_Government_Area":"Ballarat","Offence_Division":"A Crimes against the person","Offence_Subdivision":"A10 Homicide and related offences","Offence_Subgroup":"A10 Homicide and related offences","Incidents_Recorded":2,"PSA_Rate_per_100k_population":1.7,"LGA_Rate_per_100k_population":26.7},
-                 
-                 {"Year":2020,"Year_ending":"December","Police_Service_Area":"Pyrenees","Local_Government_Area":"Ballarat","Offence_Division":"A Crimes against the person","Offence_Subdivision":"A20 Assault and related offences","Offence_Subgroup":"A211 FV Serious assault","Incidents_Recorded":22,"PSA_Rate_per_100k_population":18.5,"LGA_Rate_per_100k_population":294.1},
-    ]
+    """
+    Let's read in the csv data and put into a list to read into
+    our newly created table
+    """
+    seed_data = list()
+
+    with open('data/sample.csv', newline='') as input_file:
+        reader = csv.DictReader(input_file)       #csv.reader is used to read a file
+        for row in reader:
+            seed_data.append(row)
     
+    """
+    With our newly created table let's insert the row we've read in
+    and with that we're done
+    """
     with engine.connect() as conn:
         conn.execute(new_table.insert(), seed_data)
 
     print("Seed Data Imported")
 else:
     print("Table already exists")
+
 print("initdb complete")
