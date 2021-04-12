44aa135a17fcf41a0b8c6d57c37f1e7ad2496724
pizarro84 <Raphael.p.m.serrano@gmail.com>
2021-04-12 19:20:48 +1000

    line up drop down in one row

--- static/js/app.js
+++ static/js/app.js
@@ -70,89 +70,8 @@ function populateOffenceFilter() {
   });
 }
 
-// function refreshCharts(event) {
-//   // event.target will refer tp the selector
-//   // from which we will get the selected option
-//   var selectedValue = d3.select(event.target).property('value');
-
-//   // With the selectedValue we can refresh the charts
-//   // filtering if needed. 
-//   buildRacesPieChart(selectedValue);
-//   buildRacesByClassBarChart(selectedValue);
-// }
-
-// function buildRacesPieChart(selectedRace) {
-//   // If we have race to filter by let's pass it
-//   // in as a querystring parameter
-//   var url = "api/count_by/race/char_class";
-//   if (selectedRace != undefined) {
-//     url = `api/count_by/race/char_class?race=${selectedRace}`;
-//   }
-
-//   d3.json(url).then(function(response) {
-//     // In order to render a pie chart we need to 
-//     // extract the labels and values from the 
-//     // json response. For an example see:
-//     // https://plotly.com/javascript/pie-charts/ 
-//     var data = [{
-//       labels: response.map(d => d.char_class),
-//       values: response.map(d => d.total),
-//       type: 'pie'
-//     }];
-    
-//     var layout = {
-//       height: 400,
-//       width: 500
-//     };
-    
-//     Plotly.newPlot('character-races-plot', data, layout);
-
-//   });
-// }
-
-
-// function buildRacesByClassBarChart(selectedRace) {
-//   var url = "api/count_by/race/char_class";
-//   if (selectedRace != undefined) {
-//     url = `api/count_by/race/char_class?race=${selectedRace}`;
-//   }
-
-//   d3.json(url).then(function(response) {
-
-//     // Using the group method in d3 we can
-//     // do grouping of the received json
-//     // for the purposes of creating multiple traces.
-//     // https://github.com/d3/d3-array#group
-//     var grouped_data = d3.group(response, d => d.race)
-
-//     var traces = Array();
-
-//     // then iterating over each group by it's
-//     // key we can create a trace for each group
-//     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-//     grouped_data.forEach(element => {      
-//       traces.push({
-//         x: element.map(d => d.char_class),
-//         y: element.map(d => d.total),
-//         name: element[0].race,
-//         type: 'bar'
-//       });
-//     });
-    
-//     var layout = {
-//       barmode: 'stack',
-//       height: 400,
-//       width: 500
-//     };
-    
-//     Plotly.newPlot('races-by-class-plot', traces, layout);
-//   });
-// }
-
 // Upon intial load of the page setup
 // the visualisations and the select filter
 populateYearFilter();
 populateLgaFilter();
 populateOffenceFilter();
-// buildRacesPieChart();
-// buildRacesByClassBarChart();
